766f052c6f15dca7bf44c1801bc67e5e2d53a650
aakarshan-raj <aakarshan7raj@gmail.com>
2023-06-06 00:22:54 +0530

    Most of it Done

--- src/App.js
+++ src/App.js
@@ -4,19 +4,35 @@ import './App.css';
 function App() {
   const[value,setValue] = useState("");
   const[taskList,setTask] = useState([]);
+  const[taskNumber,setTaskNumber] = useState(0);
 
   const new_task = (to_append) =>{
       setValue(to_append.target.value)
   }
   const append = ()=>{
-    setTask((old_list)=>[...old_list,value])
+    const data = {
+      id:taskNumber,
+      task:value
+    }
+    setTaskNumber(taskNumber+1);
+    setTask((old_list)=>[...old_list,data])
+    setValue("")
+  }
+
+  const deleteTask = (task_id) =>{
+     
+     setTask(taskList.filter((item)=> item.id !== task_id))
+   
   }
 
   return (
     <div className="App">
-     <input placeholder='Task!' onChange={new_task}></input> <button onClick={append}>Add</button>
-     <div>
-      {taskList.map((item)=>{ return <h1>{item}</h1> } ) }
+      <div className='addTask'>
+     <input placeholder='Task!' onChange={new_task} value={value}/>
+     <button onClick={append}>Add Task</button>
+     </div>
+     <div className='list'>
+      {taskList.map((item)=>{ return <div><h1>{item.id}. {item.task}<button onClick={()=>{deleteTask(item.id)}}>x</button></h1> </div> } ) }
      </div>
     </div>
   );
